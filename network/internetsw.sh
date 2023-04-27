727926675cfc1423ca572e9e5c0f8ce3bd322bac
Laxman Singh <jodhpur.laxman@gmail.com>
2023-04-27 22:10:27 +0530

    Update internetsw.sh

--- network/internetsw.sh
+++ network/internetsw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 status1=$(ping -c1 google.com | grep "1 received" | awk '{print $4$5}'| sed 's/\(.*\),/\1 /')
-status2=$(ping -c1 yahoo.com | grep "1 received" | awk '{print $4$5}'| sed 's/\(.*\),/\1 /')
+#status2=$(ping -c1 yahoo.com | grep "1 received" | awk '{print $4$5}'| sed 's/\(.*\),/\1 /')
 Current_GW=$(ip route | grep default | awk '{print $3}')
 OS=$(cat /etc/os-release | sed -n '1p' | awk -F "=" '{print $2}')
 
