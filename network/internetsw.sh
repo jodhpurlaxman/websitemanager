38c9106b814458494a6af6a24185177ce615692f
Laxman Singh <jodhpur.laxman@gmail.com>
2023-04-27 22:02:18 +0530

    Update internetsw.sh

--- network/internetsw.sh
+++ network/internetsw.sh
@@ -6,16 +6,16 @@ Current_GW=$(ip route | grep default | awk '{print $3}')
 OS=$(cat /etc/os-release | sed -n '1p' | awk -F "=" '{print $2}')
 
 
-if [ $status1 != "1received" ] &&  [ $status2 != "1received" ]; then
-		if [ $OS == '"Ubuntu"' ];then 
-			if [ $Current_GW == "192.168.0.1" ]; then
+if [ "$status1" != "1received" ] then
+		if [ "$OS" == '"Ubuntu"' ];then 
+			if [ "$Current_GW" == "192.168.0.1" ]; then
 				sed -i 's/192.168.0.1/192.168.0.2/g' /etc/netplan/01-netcfg.yaml
 				netplan apply
 			else
 			sed -i 's/192.168.0.2/92.168.0.1/g' /etc/netplan/01-netcfg.yaml
 			netplan apply
 			fi
-		elif [ $OS != '"Ubuntu"' ]; then 
+		elif [ "$OS" != '"Ubuntu"' ]; then 
 			if [ $Current_GW == "192.168.0.1" ]; then
 				sed -i 's/192.168.0.1/192.168.0.2/g' /etc/network/interfaces
 				ifdown etho && ifup eth0
